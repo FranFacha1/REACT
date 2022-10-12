89a79917c625d3c73c58b707fb5c178e958a50f0
francisco bellotti <franciscobellotti2002@gmail.com>
2022-10-12 20:29:44 -0300

    Primer Preentrega

---
+++ appreact/src/App.js
@@ -0,0 +1,12 @@
+
+import Navbar from './componentes/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ appreact/src/componentes/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+    const src = "https://bellottisrl.com.ar/wp-content/uploads/2019/05/logo_Bellotti_cont-1.png"
+    return (
+        <nav>
+            <div className= "div1">
+            
+                <img src={src}/>
+
+            </div>
+
+            
+            <div>
+                <button>Incio</button>
+                <button>Compras</button>
+                <button>Productos</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
